dfe2528431b4c6a505d89b1518c12ad6dab2c104
kalva karthik <210303105615@paruluniversity.ac.in>
2025-08-22 19:28:10 +0530

    template for alumni profile fecthing created

--- src/main/java/com/FreightFox/LinkedInAluminiProfile/Service/AlumniProfileService.java
+++ src/main/java/com/FreightFox/LinkedInAluminiProfile/Service/AlumniProfileService.java
@@ -1,4 +1,27 @@
 package com.FreightFox.LinkedInAluminiProfile.Service;
 
+import com.FreightFox.LinkedInAluminiProfile.Entity.AlumniProfile;
+import com.FreightFox.LinkedInAluminiProfile.Repository.AlumniProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AlumniProfileService {
+    private final AlumniProfileRepository alumniProfileRepository;
+    private final PhantomBusterService phantomBusterService;
+
+    public AlumniProfileService(AlumniProfileRepository alumniProfileRepository, PhantomBusterService phantomBusterService) {
+        this.alumniProfileRepository = alumniProfileRepository;
+        this.phantomBusterService = phantomBusterService;
+    }
+    public List<AlumniProfile> getAlumniProfilesInDB(){
+        return alumniProfileRepository.findAll();
+    }
+
+//    public List<AlumniProfile> searchAndSaveAlumniProfiles(String university, String designation, String passoutYear){
+//        List<AlumniProfile> results = phantomBusterService.searchAlumniProfiles(university, designation, passoutYear);
+//        return alumniProfileRepository.saveAll(results);
+//    }
+
 }
