71b3b5da746d583a87181cb2dd13d6cd2f9a22f0
kalva karthik <210303105615@paruluniversity.ac.in>
2025-08-22 19:24:35 +0530

    created a payload DTO to handle the incoming webhook payload

--- src/main/java/com/FreightFox/LinkedInAluminiProfile/DTO/PhantomWebhookPayload.java
+++ src/main/java/com/FreightFox/LinkedInAluminiProfile/DTO/PhantomWebhookPayload.java
@@ -1,4 +1,25 @@
 package com.FreightFox.LinkedInAluminiProfile.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhantomWebhookPayload {
+
+    private String agentId;
+    private String agentName;
+    private String containerId;
+    private String script;
+    private String scriptOrg;
+    private String branch;
+    private int launchDuration;
+    private int runDuration;
+    private Object resultObject;
+    private String exitMessage;
+    private int exitCode;
+
 }
