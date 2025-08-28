ffd1e44120b6b499d99ae3ddcf421dab56ba0311
kalva karthik <210303105615@paruluniversity.ac.in>
2025-08-28 13:24:28 +0530

    testing under process

--- src/main/java/com/KKStands/LinkedInAlumniProfile/Controller/AlumniProfileController.java
+++ src/main/java/com/KKStands/LinkedInAlumniProfile/Controller/AlumniProfileController.java
@@ -45,11 +45,9 @@ public class AlumniProfileController {
     public ResponseEntity<String> handlePhantomWebhook(@RequestBody PhantomWebhookPayload payload) {
         try {
             log.info("Webhook received for agentId: {}", payload.getAgentId());
-            log.info("Payload Which is received (RAW): \n{}", payload);
-            log.info("(RAW) resultObject: \n{}", payload.getResultObject());
+            log.info("Payload Which is received");
             if (payload.getResultObject() == null) return ResponseEntity.badRequest().body("Missing resultObject");
             alumniProfileService.saveAlumniFromWebhookResult(payload.getResultObject());
-//            log.info("saved the alumni details successfully");
             return ResponseEntity.ok("Webhook processed successfully");
         } catch (Exception e) {
             log.error("Error processing webhook payload", e);
